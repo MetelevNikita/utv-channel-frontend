1a852ac4a75a2034814bc2af9e4850141fcd1ba8
MetelevNikita <Kyle.B@mail.ru>
2024-05-16 15:14:24 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FC } from 'react'
+
+// styles
+
 import './App.css';
 
-function App() {
+//
+
+import { Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// components
+
+import Header from './components/page/Header';
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+
+        <Header />
+
+
+
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// router
+
+import { BrowserRouter } from 'react-router-dom';
+
+// redux
+
+import { Provider } from 'react-redux';
+import store from './store/store';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+
+    </BrowserRouter>
+  </Provider>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
